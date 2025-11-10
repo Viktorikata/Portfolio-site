d058de9822dbf7ea70b7f1f01a1574645171a619
Viktoria <va.2816275@yandex.com>
2025-11-10 14:01:16 +0300

    Фикс ошибки при обновлении страницы

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './index.css';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(
-      <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
-          <App />
-        </BrowserRouter>
-      </React.StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
+
 
